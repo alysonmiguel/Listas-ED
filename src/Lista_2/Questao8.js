422228d02eb2e307eb60a2666e3212edb7f3b0f2
alysonmiguel <alysonmiguel16@gmail.com>
2020-03-14 15:06:04 -0300

    Questão 8

--- src/Lista_2/Questao8.js
+++ src/Lista_2/Questao8.js
@@ -2,9 +2,10 @@ import ListaDuplamenteLigada from "../ListaDuplamenteLigada";
 
 export default function converterLissta(lista){
     let listaDL = new ListaDuplamenteLigada();
-        while(!lista.isEmpty()){
-            listaDL.add(lista.removeEnd())
-        }    
+       
+    while(!lista.isEmpty()){
+        listaDL.add(lista.removeEnd())
+    }    
         
-        return listaDL;
-    }
+    return listaDL;
+}
