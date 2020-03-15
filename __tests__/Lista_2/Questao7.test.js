30311f0e57e3368c7c6c7921a8c955851b0439da
alysonmiguel <alysonmiguel16@gmail.com>
2020-03-15 10:37:59 -0300

    Questão 7 incompleta

--- __tests__/Lista_2/Questao7.test.js
+++ __tests__/Lista_2/Questao7.test.js
@@ -2,5 +2,5 @@ import escolherPosicao from "../../src/Lista_2/Questao7"
 
 
 test("Josephus", () =>{
-    expect(escolherPosicao(3,5)).toBe("4") 
+    expect(escolherPosicao(3,5)).toBe(4) 
 })
--- src/Lista_2/Questao7.js
+++ src/Lista_2/Questao7.js
@@ -3,14 +3,17 @@ import Lista from "../Lista";
 export default function escolherPosicao(remocaoSaldado, qtdSoldado){
 
     let lista = new Lista();
+    let anterior = lista.head;
+    let atual = lista.head.proximo;
+    let i = 1;  
+
+    if(remocaoSaldado == 1){
+        return qtdSoldado;
+    }
 
     for (let i = 1; i <= qtdSoldado;  i++) {
         lista.append(i);
     }
-    
-    let anterior = lista.head;
-    let atual = lista.head.proximo;
-    let i = 1;  
 
     while (lista.size() != 1){
         console.log(lista.size());
@@ -24,73 +27,9 @@ export default function escolherPosicao(remocaoSaldado, qtdSoldado){
                 anterior = atual;
 				atual = atual.proximo;
                 i++
-                console.log("else"+ i);
             }            
         }
         atual = lista.head.proximo;
     }
-    return lista.toString() ;
+    return atual.dado;
 }
-
-// while (lista.size() != 1){
-//     console.log(lista.size());
-//     while(atual.proximo != null){
-//         if(i == remocaoSaldado){
-//             let tem = atual.dado;
-//             console.log(atual);
-//             atual = atual.proximo;
-//             lista.removeDado(tem);  
-//             console.log(atual);
-//             i = 1
-//         }else{   
-//             anterior = atual;
-//             atual = atual.proximo;
-//             i++
-//             console.log("else"+ i);
-//         }            
-//     }
-//     atual = lista.head;
-// }
-
-// while (lista.size() != 1){
-//     console.log(lista.size());
-//     while(atual.proximo != null){
-//         if(i == remocaoSaldado){
-//             console.log("aasaaaa");
-//             anterior.proximo = atual.proximo;
-//             atual = atual.proximo;  
-//             i = 0
-//             break
-//         }else{   
-//             atual = atual.proximo;
-//             console.log("bbbbbbbbbbbbbb"+ i);   
-//             i++
-//             console.log("bbbbbbbbbbbbbb"+ i);   
-
-//         }
-//     }
-//     atual = lista.head.proximo;
-// }
-
-
-// while (lista.size() != 1){
-//     console.log(lista.size());
-//     if(i == remocaoSaldado){
-//         console.log("aasaaaa");
-//         if(atual.proximo == null){
-//             atual = lista.head;
-//         }
-//         anterior.proximo = atual.proximo;
-//         atual = atual.proximo;  
-//         i = 1
-//     }else{   
-//         anterior = atual;
-//         if(atual.proximo == null){
-//             atual = lista.head;
-//         }else{
-//             atual = atual.proximo;
-//         }
-//         i++
-//         console.log("else"+ i);
-//     }
-// }
